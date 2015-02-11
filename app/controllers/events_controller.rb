5c115d452c3fa27e818197b3a8c41ba718c4570e
Andrew James <andrewpjames87@gmail.com>
2015-02-11 14:46:23 -0700

    Spec user can delete an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -35,9 +35,11 @@ class EventsController < ApplicationController
   end
 
   def destroy
+    @location = Location.find(params[:location_id])
     @event = Event.find(params[:id])
-    @event.destroy
-    redirect_to location_path, :notice => 'Location was successfully destroyed.'
+    if @event.destroy
+      redirect_to location_path(@location), notice: "Event was successfully deleted"
+    end
   end
 
   def event_params
--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -89,4 +89,31 @@ describe 'User can CRUD events' do
 
   end
 
+  scenario 'User can delete an event' do
+
+    visit '/'
+
+    click_on "New Location"
+
+    fill_in 'location[name]', :with => "g6"
+    fill_in 'location[address]', with: "New Address"
+    fill_in 'location[zipcode]', with: "90210"
+
+    click_on "Create Location"
+    expect(page).to have_content("Location was successfully created")
+
+    click_on 'Add Event'
+    expect(page).to have_content("New Event")
+
+    fill_in 'event[description]', with: "shindig"
+    fill_in 'event[date]', with: "07/11/1989"
+    check 'event[requires_id]'
+
+    click_on 'Create Event'
+    expect(page).to have_content("Event was successfully created")
+
+    click_on 'Delete'
+    expect(page).to have_content("Event was successfully deleted")
+
+  end
 end
