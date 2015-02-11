2aa3422d4b2b30b58611226344b25c86ebf4ee11
Andrew James <andrewpjames87@gmail.com>
2015-02-11 14:31:43 -0700

    Spec user can visit event show page

--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -27,5 +27,30 @@ describe 'User can CRUD events' do
 
   end
 
+  scenario 'User can visit event show page' do
+    visit '/'
+
+    click_on "New Location"
+
+    fill_in 'location[name]', :with => "g6"
+    fill_in 'location[address]', with: "New Address"
+    fill_in 'location[zipcode]', with: "90210"
+
+    click_on "Create Location"
+    expect(page).to have_content("Location was successfully created")
+
+    click_on 'Add Event'
+    expect(page).to have_content("New Event")
+
+    fill_in 'event[description]', with: "shindig"
+    fill_in 'event[date]', with: "07/11/1989"
+    check 'event[requires_id]'
+
+    click_on 'Create Event'
+    expect(page).to have_content("Event was successfully created")
+
+    click_on 'shindig'
+    expect(page).to have_content("shindig")
+  end
 
 end
